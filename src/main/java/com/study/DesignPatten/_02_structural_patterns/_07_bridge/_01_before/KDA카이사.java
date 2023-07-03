62d379650d1077b4895d5e6074c7ad92e92b0f8b
mbshin <mbshin@swm.ai>
2023-07-03 16:46:04 +0900

    feat: #13 브릿지 패턴
    브릿지 패턴
    
    close #13

---
+++ src/main/java/com/study/DesignPatten/_02_structural_patterns/_07_bridge/_01_before/KDA카이사.java
@@ -0,0 +1,28 @@
+package com.study.DesignPatten._02_structural_patterns._07_bridge._01_before;
+
+public class KDA카이사 implements Champion{
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void skillQ() {
+
+    }
+
+    @Override
+    public void skillW() {
+
+    }
+
+    @Override
+    public void skillE() {
+
+    }
+
+    @Override
+    public void skillR() {
+
+    }
+}
